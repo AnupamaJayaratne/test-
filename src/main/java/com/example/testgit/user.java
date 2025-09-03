ecb0dabb8436c742a42ed4ddd3ad61ad2e6e5fac
AnupamaJayaratne <dev.anupamajayaratne@gmail.com>
2025-09-03 11:27:43 +0530

    new

--- src/main/java/com/example/testgit/user.java
+++ src/main/java/com/example/testgit/user.java
@@ -5,5 +5,14 @@ public class user {
     private int age;
     private String email;
     public user(String name, int age, String email) {}
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
 
 }
