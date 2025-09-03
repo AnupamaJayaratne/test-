7658999885d669d9c889a964855f1cd0cfa3020c
AnupamaJayaratne <dev.anupamajayaratne@gmail.com>
2025-09-03 11:18:47 +0530

    user class

---
+++ src/main/java/com/example/testgit/user.java
@@ -0,0 +1,9 @@
+package com.example.testgit;
+
+public class user {
+    private String name;
+    private int age;
+    private String email;
+    public user(String name, int age, String email) {}
+
+}
